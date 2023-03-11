0979ab2468c8f1ef9df18e5a4a266cab4ba76811
julian <julianschriedel@gmail.com>
2023-03-11 15:52:30 -0600

    studio 6.4 complete

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -22,14 +22,19 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
-        for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
+        // this method counts opening and closing brackets to ensure there are
+        // the same amount of each (balanced brackets)
+        int brackets = 0; // set brackets counter to 0
+        for (char ch : str.toCharArray()) { // for each character in character array
+            if (ch == '[') { // if character is opening bracket
+                brackets++; // add it to counter
+            } else if (ch == ']') { // else if character is closing bracket
+                brackets--; // subtract from the counter
+            } // the method fails test #9 on reversed brackets
+            if (brackets < 0) { // if the counter is negative (more "]" than "[")
+                return false; // then return false and stop the loop
             }
         }
-        return brackets == 0;
+        return brackets == 0; // returns t/f whether there are the same number of brackets
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,143 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
+    // POSITIVE TEST CASES
+    // Does the program work as expected with valid input?
+    // TODO: TEST #1 - refactor sample test from instructions to use AAA pattern and message parameter
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        // include a spec to give info on what the test includes
+        String spec =  "a single set of balanced brackets returns true";
+        // include test data to give the method
+        String testData = "[]";
+        // where the result is the test condition
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        // using a constructor that will print a message before the test
+        assertTrue(spec, result);
     }
 
+    // TODO: TEST #2 - test balanced brackets in front of other characters - "[]LaunchCode"
+    @Test
+    public void pairOfBracketsBeforeOtherCharacters() {
+        String spec =  "pair of brackets before other characters returns true";
+        String testData = "[]LaunchCode";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
 
+    // TODO: TEST #3 - test balanced brackets around other characters - "[LaunchCode]"
+    @Test
+    public void pairOfBracketsAroundOtherCharacters() {
+        String spec =  "pair brackets around other characters returns true";
+        String testData = "[LaunchCode]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
+
+    // TODO: TEST #4 - test balanced brackets within other characters - "Launch[Code]"
+    @Test
+    public void pairOfBracketsWithinOtherCharacters() {
+        String spec =  "pair of brackets within other characters returns true";
+        String testData = "Launch[Code]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
+
+    // TODO: TEST #5 - test multiple connecting pairs of balanced brackets - "[][][]"
+    @Test
+    public void connectingPairsOfBrackets() {
+        String spec =  "multiple connecting pairs of balanced brackets returns true";
+        String testData = "[][][]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
+
+    // TODO: TEST #6 - test multiple nested brackets - "[[[]]]"
+    @Test
+    public void nestedPairsOfBrackets() {
+        String spec =  "multiple pairs of nested balanced brackets returns true";
+        String testData = "[[[]]]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
+
+    // NEGATIVE TEST CASES
+    // Does the program work as expected with invalid input?
+
+    // TODO: TEST #7 - test single opening bracket - "["
+    @Test
+    public void singleOpeningBracketReturnsFalse() {
+        String spec =  "single opening bracket returns false";
+        String testData = "[";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+
+    // TODO: TEST #8 - test single closing bracket - "]"
+    @Test
+    public void singleClosingBracketReturnsFalse() {
+        String spec = "single closing bracket returns false";
+        String testData = "]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+
+    // TODO: TEST #9 - test reversed brackets - "]["
+    @Test
+    public void reversedPairOfBracketsReturnsFalse() {
+        String spec = "single pair of reversed brackets returns false";
+        String testData = "][";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+    // initially test #9 fails, so we have to edit the code in BalancedBrackets
+
+    // TODO: TEST #10 - test single bracket before other characters - "[LaunchCode"
+    @Test
+    public void singleBracketBeforeOtherCharacters() {
+        String spec = "single bracket before other characters returns false";
+        String testData = "[LaunchCode";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+
+    // TODO: TEST #11 - unbalanced brackets amoung other characters - "Launch]Code["
+    @Test
+    public void unbalancedBracketsAmongOtherCharacters() {
+        String spec = "pair of unbalanced brackets among other characters returns false";
+        String testData = "Launch]Code[";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+
+    // TODO: TEST #12 - test a series of mixed unbalanced brackets - "[[]][]["
+    @Test
+    public void seriesUnbalancedBrackets() {
+        String spec = "series of unbalanced brackets returns false";
+        String testData = "[[]][][";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertFalse(spec, result);
+    }
+
+    // TEST EDGE CASES
+    // Testing the ends (or edges) of the range of valid inputs
+
+    // TODO: TEST #13 - test string with only characters - "LaunchCode"
+    @Test
+    public void onlyCharacters() {
+        String spec = "string with only characters returns true";
+        String testData = "LaunchCode";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
+
+    // TODO: TEST #14 - test empty string - ""
+    @Test
+    public void emptyString() {
+        String spec = "empty string returns false";
+        String testData = "";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(spec, result);
+    }
 }
